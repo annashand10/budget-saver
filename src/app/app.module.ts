638a5e0b04023354964d68a1c4032c16d37336a8
annashand10 <annashand10@gmail.com>
2018-03-21 10:07:34 +0200

    refactoring directories and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,56 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {HeaderModule} from './header/header.module';
-import { UiComponentsComponent } from './components/ui-components.component';
-import { QuickBudgetComponent } from './my-budget/quick-budget/quick-budget.component';
-import { SavingsComponent } from './my-budget/savings/savings.component';
-import { TotalSalaryComponent } from './my-budget/total-salary/total-salary.component';
-import { TableComponent } from './components/table/table.component';
-import { TooltipComponent } from './components/tooltip/tooltip.component';
-import { MyBudgetComponent } from './my-budget/my-budget.component';
-import { AccountComponent } from './account/account.component';
-import { ReportsComponent } from './reports/reports.component';
-import { ServicesComponent } from './services/services.component';
-import { AuthComponent } from './auth/auth.component';
-import { LayoutComponent } from './layout/layout.component';
-import { NewBudgetPanelComponent } from './new-budget-panel/new-budget-panel.component';
-import { CalendarComponent } from './components/calendar/calendar.component';
-import { DropdownComponent } from './components/dropdown/dropdown.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
-
+import {AccountModule} from './account/account.module';
+import {AuthModule} from './auth/auth.module';
+import {LayoutModule} from './layout/layout.module';
+import {MyBudgetModule} from './my-budget/my-budget.module';
+import {ReportsModule} from './reports/reports.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderModule,
-    UiComponentsComponent,
-    QuickBudgetComponent,
-    SavingsComponent,
-    TotalSalaryComponent,
-    TableComponent,
-    TooltipComponent,
-    MyBudgetComponent,
-    AccountComponent,
-    ReportsComponent,
-    ServicesComponent,
-    AuthComponent,
-    LayoutComponent,
-    NewBudgetPanelComponent,
-    CalendarComponent,
-    DropdownComponent,
-    LoginComponent,
-    SignupComponent,
-    ForgotPasswordComponent
-  ],
   imports: [
-    BrowserModule
+    HeaderModule,
+    BrowserModule,
+    AccountModule,
+    AuthModule,
+    LayoutModule,
+    MyBudgetModule,
+    ReportsModule
   ],
   providers: [],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/my-budget/my-budget.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MyBudgetComponent} from './my-budget.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ MyBudgetComponent ]
+})
+export class MyBudgetModule { }
